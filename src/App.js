2751cd34eb150f8a51ccbefe89f4b8c111ffd693
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2018-06-19 17:31:57 +0530

    read expenses from db

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import { Provider } from "react-redux";
 import AppRouter from "./router/AppRouter";
 import configureStore from "./store/configureStore";
+import { startSetExpenses } from "./actions/expenses";
 import "normalize.css/normalize.css";
 const store = configureStore();
+
+store.dispatch(startSetExpenses());
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 export default jsx;
--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -37,3 +37,27 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+//Set_Expenses
+
+export const setExpenses = expenses => ({
+  type: "SET_EXPENSES",
+  expenses
+});
+export const startSetExpenses = () => {
+  return dispatch => {
+    return database
+      .ref("expenses")
+      .once("value")
+      .then(snapshot => {
+        const expenses = [];
+        snapshot.forEach(expense => {
+          expenses.push({
+            id: expense.key,
+            ...expense.val()
+          });
+        });
+        dispatch(setExpenses(expenses));
+      });
+  };
+};
