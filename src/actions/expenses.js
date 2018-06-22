7cabf5973a41aaf7b3fb24645776ddc7f1680820
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2018-06-22 20:37:25 +0530

    store data for diff users

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -6,7 +6,8 @@ export const addExpense = expense => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = "",
       note = "",
@@ -15,7 +16,7 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
     database
-      .ref("expenses")
+      .ref(`users/${uid}/expenses`)
       .push(expense)
       .then(ref => {
         dispatch(
@@ -45,9 +46,10 @@ export const setExpenses = expenses => ({
   expenses
 });
 export const startSetExpenses = () => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref("expenses")
+      .ref(`users/${uid}/expenses`)
       .once("value")
       .then(snapshot => {
         const expenses = [];
@@ -62,9 +64,10 @@ export const startSetExpenses = () => {
   };
 };
 export const startRemoveExpense = ({ id }) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     database
-      .ref(`expenses/${id}`)
+      .ref(`users/${uid}/expenses/${id}`)
       .remove()
       .then(snapshot => {
         dispatch(removeExpense({ id }));
@@ -73,9 +76,10 @@ export const startRemoveExpense = ({ id }) => {
 };
 
 export const startEditExpense = (id, expense) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     database
-      .ref(`expenses/${id}`)
+      .ref(`users/${uid}/expenses/${id}`)
       .update(expense)
       .then(snapshot => {
         dispatch(editExpense(id, expense));
